caf9d120245b49de15d8840310e2133b25896b97
SirNouvel <110280971+SirNouvel@users.noreply.github.com>
2025-04-05 16:01:13 -0500

    welpp Im no longer insomniac but adderall is starting to behave like how it used too soooo that a corcern anywayy IM backk babyy

--- temp/public/javascripts/dropdown.js
+++ temp/public/javascripts/dropdown.js
@@ -1,14 +1,14 @@
 
 window.addEventListener('load', function () {
     // wait until the page loads before working with HTML elements
-    document.addEventListener('click', function (event) {
+    document.addEventListener('touchstart', function (event) {
         //click listener on the document
         document.querySelectorAll('.dropdown-content').forEach(function (el) {
             if (el !== event.target) el.classList.remove('show')
             // close any showing dropdown that isn't the one just clicked
         });
         if (event.target.matches('.drop-button')) {
-            event.target.closest('.drop-down').querySelector('.dropdown-content').classList.toggle('show')
+            event.target.closest('.drop-down').querySelector('.dropdown-content').classList.toggle('showX')
         }
         // if this is a dropdown button being clicked, toggle the show class
     })
