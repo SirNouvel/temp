11c25c54f0525c52edd26402a72ba8a943b0c784
SirNouvel <110280971+SirNouvel@users.noreply.github.com>
2025-07-22 14:22:59 -0500

    my friend are more autsitic than me and it shows COIUGH COUGH YULI

--- temp/routes/index.js
+++ temp/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('test', { title: 'Express' });
 });
 
 router.get('/info', function (req, res) {
